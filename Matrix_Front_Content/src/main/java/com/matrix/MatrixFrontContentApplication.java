bb4dfa4a4fdb2fb5b66bd2af5222cf1f6117f3a2
MentalKali <jasonfu516@outlook.com>
2018-09-02 00:50:25 +0800

    Script auto commit

--- Matrix_Front_Content/src/main/java/com/matrix/MatrixFrontContentApplication.java
+++ Matrix_Front_Content/src/main/java/com/matrix/MatrixFrontContentApplication.java
@@ -2,8 +2,10 @@ package com.matrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MatrixFrontContentApplication {
 
 	public static void main(String[] args) {
