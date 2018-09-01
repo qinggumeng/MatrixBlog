e8323496a24abcb8582f83dfa12d618a9432e204
MentalKali <jasonfu516@outlook.com>
2018-09-01 23:40:22 +0800

    Script auto commit

---
+++ Matrix_Front_Content/src/main/java/com/matrix/MatrixFrontContentApplication.java
@@ -0,0 +1,12 @@
+package com.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixFrontContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixFrontContentApplication.class, args);
+	}
+}
