9013153da321605d4c5c7a0e35798fa5d99e813b
MentalKali <jasonfu516@outlook.com>
2018-09-06 15:55:22 +0800

    Script auto commit

--- Matrix_Front_Content/src/main/java/com/matrix/dao/ContentMapper.java
+++ Matrix_Front_Content/src/main/java/com/matrix/dao/ContentMapper.java
@@ -3,11 +3,14 @@ package com.matrix.dao;
 import com.matrix.pojo.Content;
 import com.matrix.pojo.ContentExample;
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
 /**
  * 文章实体映射接口
  * @author JasonFu
  * @createTime 2018年9月6日 下午1:59:40
  */
+@Repository
 public interface ContentMapper {
     int deleteByPrimaryKey(Long id);
     int insert(Content record);
