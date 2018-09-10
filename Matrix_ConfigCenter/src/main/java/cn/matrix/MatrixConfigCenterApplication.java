9c902eb51d23b1b0dd6eb5c5ca86e23f2fd2a9d5
MentalKali <jasonfu516@outlook.com>
2018-09-10 23:23:00 +0800

    Script auto commit

--- Matrix_ConfigCenter/src/main/java/cn/matrix/MatrixConfigCenterApplication.java
+++ Matrix_ConfigCenter/src/main/java/cn/matrix/MatrixConfigCenterApplication.java
@@ -3,8 +3,6 @@ package cn.matrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableConfigServer
